64236e31484fb97251a1b8d35c88c947c7d5365e
Harã Heique dos Santos <heikacademicos@hotmail.com>
2018-11-11 05:11:59 -0200

    Functions sql

--- Queries/functions.sql
+++ Queries/functions.sql
@@ -1,44 +1,72 @@
-﻿SELECT p.nome AS Titular, po.nome AS "Nome do posto", me.nome AS Medicamento, s.quantidademedicamento "Quantidade de Medicamentos" FROM pessoa AS p
-    INNER JOIN titular AS t ON (p.idpessoa = t.idpessoa)
-    INNER JOIN solicitacao AS s ON (s.idtitular = t.idpessoa)
-    INNER JOIN medicamento_posto AS mp ON (mp.idmedicamentoposto = s.idmedicamentoposto)
-    INNER JOIN posto AS po ON (po.idposto = mp.idposto)
-    INNER JOIN medicamento AS me ON (me.idmedicamento = mp.idmedicamento)
-    ORDER BY p.nome, po.nome
+﻿DROP FUNCTION IF EXISTS get_solicitacoes_pelo_estado(int, int);
+DROP FUNCTION IF EXISTS verifica_qntsolicitacao_pelo_estado(int, int);
+DROP FUNCTION IF EXISTS medicamentos_postos(idPosto INTEGER);
+DROP FUNCTION IF EXISTS quantidade_recebimento_por_atendente(idPosto INTEGER);
+DROP FUNCTION IF EXISTS filtrar_pessoa(VARCHAR(30));
+DROP FUNCTION IF EXISTS filtrar_dependentes_pelo_idtitular(idTitular INTEGER);
 
-select * from solicitacao limit 10;
-select * from titular
-select * from medicamento_posto
+-- Pega todos os campos da solicitação passando como parâmetro o estado da solitação e o limite de linhas retornadas
+CREATE OR REPLACE FUNCTION get_solicitacoes_pelo_estado(int, int)
+RETURNS SETOF solicitacao AS 
+	'SELECT * FROM solicitacao WHERE estadosolicitacao=$1 LIMIT $2'
+LANGUAGE SQL;
 
-    create index fk_idtitular on solicitacao (idtitular)
-    
-    create index fk_idmedicamentoposto on solicitacao (idmedicamentoposto)
+-- Pega a quantidade de medicamentos solicitados pelo Titular a partir do estado e a quantidade de linhas limites retornadas
+CREATE OR REPLACE FUNCTION verifica_qntsolicitacao_pelo_estado(int, int)
+RETURNS TABLE(idtitular int, qtd_solicitado bigint, estadosolicitacao int) AS $$
+	SELECT idtitular, sum(quantidademedicamento), estadosolicitacao FROM solicitacao
+	WHERE estadosolicitacao=$1
+	GROUP BY idtitular, estadosolicitacao 
+	LIMIT $2; 
+$$
+LANGUAGE SQL;
 
-    create index fk_pessoa on titular (idpessoa)
+-- Pega os medicamentos e sua quantidade em estoque a partir do id do posto
+CREATE OR REPLACE FUNCTION medicamentos_postos(idPosto INTEGER)
+RETURNS TABLE("Medicamento" VARCHAR(100), "Quantidade" INTEGER, "Data de Vencimento" TIMESTAMP) AS 
+	'SELECT me.nome, mp.quantidade, mp.datavencimento FROM medicamento AS me
+	INNER JOIN medicamento_posto AS mp ON (mp.idmedicamento = me.idmedicamento)
+	WHERE mp.idposto=$1
+	ORDER BY me.nome;'
+LANGUAGE SQL;
 
-    create index fk_idposto on medicamento_posto (idposto)
+-- Pega a quantidade de recebimentos realizados pelo atendente a partir do id do posto
+CREATE FUNCTION quantidade_recebimento_por_atendente(idPosto INTEGER)
+RETURNS TABLE("Registro Geral" VARCHAR(10), "Nome" VARCHAR(50), "Quantidade de Retiradas" BIGINT)
+AS $$
+	SELECT ate.numeroregistro, pe.nome, COUNT(re.idatendente) FROM pessoa AS pe
+	INNER JOIN atendente AS ate ON (ate.idpessoa = pe.idpessoa)
+	INNER JOIN recebimento AS re ON (re.idatendente = ate.idpessoa)
+	INNER JOIN medicamento_posto AS mp ON (mp.idmedicamentoposto = re.idmedicamentoposto)
+	WHERE mp.idposto = $1
+	GROUP BY ate.numeroregistro, pe.nome
+	ORDER BY 3 DESC;
+$$
+LANGUAGE SQL;
 
-    create index fk_idmedicamento on medicamento_posto (idmedicamento)
+-- Filtrar pessoas passando como parâmetro o nome
+CREATE FUNCTION filtrar_pessoa(VARCHAR(30))
+RETURNS SETOF pessoa 
+AS $$
+	SELECT * FROM pessoa WHERE nome=$1;
+$$
+LANGUAGE SQL;
 
+-- Filtrar os dependentes pelo id do titular
+CREATE FUNCTION filtrar_dependentes_pelo_idtitular(idTitular INTEGER)
+RETURNS TABLE("Nome" VARCHAR(40), "Sexo" INTEGER, "Data de Nascimento" TIMESTAMP)
+AS $$
+	SELECT pe.nome, pe.sexo, pe.datanascimento FROM pessoa AS pe
+	INNER JOIN titular AS ti ON(ti.idpessoa = pe.idpessoa)
+	INNER JOIN dependente AS de ON (de.idtitular = ti.idpessoa)
+	WHERE de.idtitular = $1;
+$$
+LANGUAGE SQL;
 
-DROP FUNCTION verifica_solicitacao_por_pessoa(integer)
-
-CREATE OR REPLACE FUNCTION verifica_estado_solicitacao(int)
-   RETURNS SETOF solicitacao AS 'select * from solicitacao where estadosolicitacao=$1 limit 100'
-   LANGUAGE SQL;
-
-CREATE OR REPLACE FUNCTION verifica_solicitacao_por_pessoa(int)
-   RETURNS table(idtitular int, qtd_solicitado bigint, estadosolicitacao int) AS $$ select idtitular, sum(quantidademedicamento), estadosolicitacao from solicitacao where estadosolicitacao=$1 
-	group by idtitular, estadosolicitacao limit 100; $$
-   LANGUAGE SQL;
-
-   select idtitular, sum(quantidademedicamento) from solicitacao where estadosolicitacao=1 
-	group by idtitular limit 100
-   
-    
-SELECT * FROM verifica_estado_solicitacao(1);
-SELECT * FROM verifica_solicitacao_por_pessoa(1);
-
-
-
-    
+-- Selects das functions	
+SELECT * FROM get_solicitacoes_pelo_estado(2, 150);
+SELECT * FROM verifica_qntsolicitacao_pelo_estado(1, 200);
+SELECT * FROM medicamentos_postos(3);
+SELECT * FROM quantidade_recebimento_por_atendente(1);
+SELECT * FROM filtrar_pessoa('Soraia Outeiro');
+SELECT * FROM filtrar_dependentes_pelo_idtitular(32514);

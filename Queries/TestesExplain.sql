7c775ea853d571b2a9dd387ec78d606fd804886a
lukasg18 <lukas.gomes2010@gmail.com>
2018-11-13 17:13:45 -0200

    adicionando arquivos no testeExplain.sql

--- Queries/TestesExplain.sql
+++ Queries/TestesExplain.sql
@@ -4,22 +4,36 @@ INNER JOIN titular AS ti ON (ti.idpessoa = pe.idpessoa)
 INNER JOIN solicitacao AS so ON (so.idtitular = ti.idpessoa)
 INNER JOIN medicamento_posto AS mp ON (mp.idmedicamentoposto = so.idmedicamentoposto)
 INNER JOIN medicamento AS me ON (me.idmedicamento = mp.idmedicamento)
-where estadosolicitacao = 1
+where estadosolicitacao = 2 and ti.idpessoa =1000 
+
+select * from pg_constraint where contype ='p'
+select * from pg_class
+
+create index index_idtitular on titular (idpessoa);
+create index index_estadosolicitacao on solicitacao (estadosolicitacao);
+create index index_pkidsolicitacao on solicitacao (idsolicitacao);
+create index index_pkpessoa on pessoa (idpessoa);
+create index index_pkmedicamentoposto on medicamento_posto (idmedicamentoposto);
+create index index_pkmedicamento on medicamento (idmedicamento);
+create index index_pkrecebimento on recebimento (idrecebimento);
+
+
+vacuum analyse
 
 /* CREATE VIEW view_medicamentos_mais_retirados_por_quantidade AS*/
 EXPLAIN ANALYSE SELECT me.nome, SUM(re.quantidademedicamentos) AS "Quantidade" FROM medicamento AS me
 INNER JOIN medicamento_posto AS mp ON (mp.idmedicamento = me.idmedicamento)
 INNER JOIN recebimento AS re ON (re.idmedicamentoposto = mp.idmedicamentoposto)
 GROUP BY me.idmedicamento
-having SUM(re.quantidademedicamentos) >= 8500 
-ORDER BY "Quantidade" DESC limit 10
+having SUM(re.quantidademedicamentos) >= 8500 and me.idmedicamento = 46
+ORDER BY "Quantidade" DESC
 
 /*CREATE VIEW view_medicamentos_mais_solicitados_por_quantidade AS*/
 EXPLAIN ANALYSE SELECT me.nome, SUM(me.idmedicamento) AS "Quantidade" FROM medicamento AS me
 INNER JOIN medicamento_posto AS mp ON (mp.idmedicamento = me.idmedicamento)
 INNER JOIN solicitacao AS so ON (so.idmedicamentoposto = mp.idmedicamentoposto)
 GROUP BY me.idmedicamento
-having SUM(me.idmedicamento) >= 1600000
+having SUM(me.idmedicamento) >= 1600000 and me.idmedicamento = 88
 ORDER BY "Quantidade" DESC;
 
 /* CREATE VIEW view_medicamentos_mais_solicitados_por_numero_solicitacoes AS */
